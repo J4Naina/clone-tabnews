86a88863b843be21500e8a3a0c91e7580aa1b9a3
janainarodrigues <112836420+J4Naina@users.noreply.github.com>
2026-03-08 21:28:10 +0000

    Alterações

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ return(
             alignItems: "center",
             height: "100vh"
         }}>
-        <h1>Olá</h1>
+        <h1>Vamos programar</h1>
         <img src="https://i.pinimg.com/originals/17/d5/40/17d54085c8cb6b3f2f0da5bd6ce6a451.gif"/>
     </div>
     );
