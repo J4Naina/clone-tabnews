08e61a160e08d5b8f188743a5561cd96ddcc4178
janainarodrigues <112836420+J4Naina@users.noreply.github.com>
2026-03-08 00:19:08 +0000

    adiciona o arquivo .gitignore

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function Home(){
+return(
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh"
+        }}>
+        <h1>Olá</h1>
+        <img src="https://i.pinimg.com/originals/17/d5/40/17d54085c8cb6b3f2f0da5bd6ce6a451.gif"/>
+    </div>
+    );
+}
+
+export default Home
