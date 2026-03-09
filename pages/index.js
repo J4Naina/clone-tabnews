ad7b3edc1ceb5ff4426172dfbcdb12f70b73cc9d
janainarodrigues <112836420+J4Naina@users.noreply.github.com>
2026-03-09 22:30:19 +0000

    Adiciona scripts ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-function Home(){
-return(
-    <div style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh"
-        }}>
-        <h1>Vamos programar</h1>
-        <img src="https://i.pinimg.com/originals/17/d5/40/17d54085c8cb6b3f2f0da5bd6ce6a451.gif"/>
+function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Vamos programar</h1>
+      <img src="https://i.pinimg.com/originals/17/d5/40/17d54085c8cb6b3f2f0da5bd6ce6a451.gif" />
     </div>
-    );
+  );
 }
 
-export default Home
+export default Home;
